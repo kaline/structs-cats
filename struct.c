0db63795d02626cea06d7c9e3ddd2f3c4974a417
kaline <mesquitabgkaline@gmail.com>
2023-08-01 15:05:34 -0300

    update

---
+++ struct.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+typedef struct // cria uma struct para armazenar os dados de uma pessoa
+{
+    float Peso;
+    int Idade;
+    float Altura;
+
+}Pessoa;
+
+int main(){
+
+    Pessoa Joao, P1, P2;
+    Pessoa Povo[10];
+
+    Joao.Idade = 15;
+    Joao.Peso = 60.5;
+    Joao.Altura = 1.68;
+
+    printf("Joao  \nIdade: %d\n", Joao.Idade);
+
+    return 0;
+
+}
+
